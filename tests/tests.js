b8b020bf99c4b08c0674a6ecd847a24cdc80ab06
Tonny Yan <yhppenny@gmail.com>
2014-11-10 01:51:21 +1100

    initialization

---
+++ tests/tests.js
@@ -0,0 +1,15 @@
+/**
+ * Created by spdev on 10/11/2014.
+ * description : test
+ */
+var assert = require('assert');
+
+describe('Test Framework', function () {
+    it('should have mocha installed and running', function () {
+        assert.equal(true,true);
+    })
+});
+
+
+
+
